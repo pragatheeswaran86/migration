0915592069b0a985fa739abfa2fc84c4a921943f
pragatheeswaran86 <84690635+pragatheeswaran86@users.noreply.github.com>
2026-02-05 17:28:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,32 @@
+variable "org_id" {
+  type = string
+}
 
-variable "org_id" { type = string }
-variable "billing_account" { type = string }
-variable "project_id" { type = string }
-variable "region" { type = string default = "asia-south1" }
-variable "network_name" { type = string default = "microservices-vpc" }
-variable "subnet_name" { type = string default = "microservices-subnet" }
-variable "cluster_name" { type = string default = "microservices-gke" }
+variable "billing_account" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "asia-south1"
+}
+
+variable "network_name" {
+  type    = string
+  default = "microservices-vpc"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "microservices-subnet"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "microservices-gke"
+}
+``
